66b3932b420e663a253b05f3001181b70184a447
shixinghao <245978782@qq.com>
2022-01-24 16:19:01 +0800

    chore

--- miniprogram/custom-router/utils/router.js
+++ miniprogram/custom-router/utils/router.js
@@ -3,7 +3,7 @@ class Router {
   constructor(options = {}) {
     this.isVueRouter = options.isVueRouter == false ? false : true
     this.tabbars = options.tabbars || []
-    this.MAX_LIMIT = 10
+    this.MAX_LIMIT = 3
     this.routerStack = []
     this.wxRouterStack = []
     this.middlePagePath = '/pages/middle/index'
@@ -26,16 +26,93 @@ class Router {
         }
       }
       else if (openType == 'switchTab') {
-        this.routerStack = [path]
+        this.routerStack = ['/' + path]
       } else if (openType == 'navigateTo') {
-
       } else if (openType == 'navigateBack') {
-
+        this._updateRouterStack(activePath)
       }
       console.log('wx.' + openType, ': ', path)
       console.log('wx页面栈:', this.wxRouterStack)
       console.log('js页面栈:', this.routerStack)
     })
+  }
+  _updateRouterStack(path) {
+    // back调用和微信元素返回处在不同区域，需要统一化处理
+    for (let i = this.routerStack.length - 1; i >= 0; i--) {
+      if (this.routerStack[i] == path) {
+        break;
+      }
+      this.routerStack.pop()
+
+    }
+  }
+  /**
+   * 通过返回进入中间页后：
+   * 1. 页面栈 >= 微信页面栈限制时，当前微信页面栈为：MAX_LIMIT-1，空出一位置，使用 navigateTo
+   * 2. 反之，自动再返回一级
+   */
+  handleMiddleRedirect(isBack) {
+    const activePath = this._getActivePath()
+    if (activePath != this.middlePagePath) {
+      throw new Error('当前路径不是中转页')
+    }
+    if (!isBack) {
+      throw new Error('中间页非返回状态')
+    }
+    // 移除末页（返回起始页）
+    if (this.routerStack.length > this.MAX_LIMIT) {
+      this.routerStack.pop()
+      wx.navigateTo({
+        url: this.routerStack[this.routerStack.length - 1]
+      })
+    } else {
+      this.back(1)
+    }
+  }
+  back(delta = 1) {
+    if (this.routerStack.length <= 1) {
+      return;
+    }
+    delta = Math.abs(delta)
+    // 至少保留1个页面栈
+    delta = this.routerStack.length - delta <= 1 ? 1 : delta
+
+    const backUrl = this.routerStack[this.routerStack.length - 2]
+
+    if (this._isTabBar(backUrl)) {
+      wx.switchTab({ url: backUrl })
+    } else if (this.routerStack.length > this.MAX_LIMIT) {
+      // 大于页面栈的返回
+      this._updateRouterStack(backUrl)
+      wx.redirectTo({
+        url: backUrl,
+      })
+    } else {
+      wx.navigateBack({ delta })
+    }
+
+  }
+  push(route) {
+    let url = this.isVueRouter ? this._routeToWxUrl(route) : route
+    // tabbar 页面重置页面栈，使用对应 api 跳转
+    if (this._isTabBar(url)) {
+      this.routerStack = [url]
+      wx.switchTab({ url })
+      return
+    }
+    const len = this.routerStack.length;
+    let actionFn = 'navigateTo'
+    // 保存中间页
+    if (len + 1 == this.MAX_LIMIT - 1) {
+      this.routerStack.push(this.middlePagePath)
+      url = this.middlePagePath + '?url=' + encodeURIComponent(url)
+    } else {
+      this.routerStack.push(url)
+    }
+    if (this.routerStack.length > this.MAX_LIMIT) {
+      actionFn = 'redirectTo'
+    }
+    wx[actionFn]({ url })
   }
   /**
    * @description vue 路由转微信 url
@@ -73,84 +150,6 @@ class Router {
   _getActivePath() {
     const pages = getCurrentPages()
     return '/' + pages.map(page => page.route)[pages.length - 1]
-  }
-  /**
-   * 通过返回进入中间页后：
-   * 1. 页面栈 >= 微信页面栈限制时，当前微信页面栈为：MAX_LIMIT-1，空出一位置，使用 navigateTo
-   * 2. 反之，自动再返回一级
-   */
-  handleMiddleRedirect(isBack) {
-    const activePath = this._getActivePath()
-    if (activePath != this.middlePagePath) {
-      throw new Error('当前路径不是中转页')
-    }
-    if (!isBack) {
-      throw new Error('中间页非返回状态')
-    }
-    // 移除末页（返回起始页）
-    this.routerStack.pop()
-    if (this.routerStack.length >= this.MAX_LIMIT) {
-      wx.navigateTo({
-        url: this.routerStack[this.routerStack.length - 1]
-      })
-    } else {
-      wx.navigateBack({
-        delta: 1,
-      })
-    }
-  }
-  /**
-   * 中间页跳转，只有两种情况会调用：
-   * 1. 超出页面栈的返回
-   * 2. 第 MAX_LIMIT 页面的返回
-   */
-  back(delta = 1) {
-    if (this.routerStack.length <= 1) {
-      return;
-    }
-    delta = Math.abs(delta)
-    // 至少保留1个页面栈
-    delta = this.routerStack.length - delta <= 1 ? 1 : delta
-
-    const len = this.routerStack.length
-    const url = this.routerStack[len - 2]
-
-    if (this._isTabBar(url)) {
-      wx.switchTab({ url })
-      return;
-    }
-    if (url == this.middlePagePath) {
-      // 返回页自动处理
-    } else if (len > this.MAX_LIMIT) {
-      this.routerStack.splice(this.routerStack.length - delta)
-      wx.redirectTo({ url })
-    } else {
-      // navigateBack 返回中，自动减去自定义页面栈数量
-      this.routerStack.splice(this.routerStack.length - delta + 1)
-      wx.navigateBack({ delta })
-    }
-  }
-  push(route) {
-    let url = this.isVueRouter ? this._routeToWxUrl(route) : route
-    // tabbar 页面重置页面栈，使用对应 api 跳转
-    if (this._isTabBar(url)) {
-      this.routerStack = [url]
-      wx.switchTab({ url })
-      return
-    }
-    const len = this.routerStack.length;
-    let actionFn = 'navigateTo'
-    // 保存中间页
-    if (len + 1 == this.MAX_LIMIT - 1) {
-      this.routerStack.push(this.middlePagePath)
-      url = this.middlePagePath + '?url=' + encodeURIComponent(url)
-    } else {
-      this.routerStack.push(url)
-    }
-    if (this.routerStack.length > this.MAX_LIMIT) {
-      actionFn = 'redirectTo'
-    }
-    wx[actionFn]({ url })
   }
 }
 
