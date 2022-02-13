434594925186b06acee6fcd23a004be298134fd6
eminoda <245978782@qq.com>
2022-02-14 00:46:10 +0800

    chroe: icon

--- react/helloworld/src/App.js
+++ react/helloworld/src/App.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import ThemeSetting from './advance/context/ThemeSetting';
-import FunctionLifeCycle from './advance/lifecycle/FunctionLifeCycle';
+import React, { Component } from "react";
+import "./App.css";
+import ThemeSetting from "./advance/context/ThemeSetting";
+import FunctionLifeCycle from "./advance/lifecycle/FunctionLifeCycle";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Hello, World! </h1>
+        <div className="logo">
+          <div className="logo-text">
+            <div className="text1">前端</div>
+            <div className="text2">
+              90<span className="icon">′</span>
+            </div>
+            <div className="text3">NextInnovation</div>
+          </div>
+        </div>
         <ThemeSetting />
         <FunctionLifeCycle />
       </div>
